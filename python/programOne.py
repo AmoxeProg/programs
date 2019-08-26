d68fac0fb2a9bb989d9d7c2e4b26d995ab584dd7
root <root@localhost.localdomain>
2019-08-25 17:34:33 -0700

    python

---
+++ python/programOne.py
@@ -0,0 +1,9 @@
+  n= int(input())
+       if N*2/2 == N && 2<N<5
+           print('Not Weird')
+       else if N*2/2 == N && 6<N<20
+           print('Weird')
+      else if  N/2*2 == N && N>20
+            print('Weird')
+       else
+           print('Weird')
